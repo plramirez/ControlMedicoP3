7aea97aaada06905eb6d56d21a653494ab363158
Pedrito <pedroluisramirezo@gmail.com>
2020-03-31 01:23:38 -0400

    Editado el webconfig

--- ProyectoFinalP3/ProyectoFinalP3/Models/AltasMedicas.cs
+++ ProyectoFinalP3/ProyectoFinalP3/Models/AltasMedicas.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,15 @@ namespace ProyectoFinalP3.Models
 {
     public class AltasMedicas
     {
+        [Key]
+        public int IdAltaMedica { get; set; }
+        public string Paciente { get; set; }       
+        public int IdIngresos { get; set; }
+        [ForeignKey("IdIngresos")]
+        public Ingresos Ingresos { get; set; }
+        public string Habitacion { get; set; }
+        public string FechaIngreso { get; set; }
+        public string FechaSalido { get; set; }
+        public int PrecioTotal { get; set; }
     }
 }
