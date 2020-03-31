0cc04fe9b160165161396abf9a2cd851a7995401
Pedrito <pedroluisramirezo@gmail.com>
2020-03-31 01:37:23 -0400

    Comando enable-migrations ejecutado

---
+++ ProyectoFinalP3/ProyectoFinalP3/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ProyectoFinalP3.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProyectoFinalP3.Models.ControlMedicoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ProyectoFinalP3.Models.ControlMedicoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
