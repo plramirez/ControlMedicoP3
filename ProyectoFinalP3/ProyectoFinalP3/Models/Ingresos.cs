5c9a395db38d282e5021b951d6079b02165118c9
Pedrito <pedroluisramirezo@gmail.com>
2020-03-31 01:09:27 -0400

    Clase de Ingresos creada

---
+++ ProyectoFinalP3/ProyectoFinalP3/Models/Ingresos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinalP3.Models
+{
+    public class Ingresos
+    {
+        [Key]
+        public int IdIngreso { get; set; }
+        public int IdPacientes { get; set; }
+        [ForeignKey("IdPacientes")]
+        public Pacientes Pacientes { get; set; }
+        public int IdHabitaciones { get; set; }
+        [ForeignKey("IdHabitaciones")]
+        public Habitaciones Habitaciones { get; set; }
+        public string FechaIngreso { get; set; }
+    }
+}
