982395380f7cc0077ac19af907eb0c245ada6c7b
Pedrito <pedroluisramirezo@gmail.com>
2020-03-31 01:21:37 -0400

    Clase Context creada

---
+++ ProyectoFinalP3/ProyectoFinalP3/Models/ControlMedicoContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ProyectoFinalP3.Models
+{
+    public class ControlMedicoContext:DbContext
+    {
+        public ControlMedicoContext()
+                     : base("Cadena1")
+                {
+                }
+                public DbSet<Medicos> Medicos { get; set; }
+                public DbSet<Pacientes> Pacientes { get; set; }
+                public DbSet<Habitaciones> Habitaciones { get; set; }
+                public DbSet<CitasMedicas> CitasMedicas { get; set; }
+                public DbSet<Ingresos> Ingresos { get; set; }
+                public DbSet<AltasMedicas> AltasMedicas { get; set; }
+    }
+}
