d3854f850d4f72fc57e28cae7448c007fa3db19a
Pedrito <pedroluisramirezo@gmail.com>
2020-03-31 23:51:13 -0400

    Creadas las vistas y controlador de Ingresos.

---
+++ ProyectoFinalP3/ProyectoFinalP3/Controllers/IngresosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinalP3.Models;
+
+namespace ProyectoFinalP3.Controllers
+{
+    public class IngresosController : Controller
+    {
+        private ControlMedicoContext db = new ControlMedicoContext();
+
+        // GET: Ingresos
+        public ActionResult Index()
+        {
+            var ingresos = db.Ingresos.Include(i => i.Habitaciones).Include(i => i.Pacientes);
+            return View(ingresos.ToList());
+        }
+
+        // GET: Ingresos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingresos ingresos = db.Ingresos.Find(id);
+            if (ingresos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingresos);
+        }
+
+        // GET: Ingresos/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdHabitaciones = new SelectList(db.Habitaciones, "IdHabitacion", "Numero");
+            ViewBag.IdPacientes = new SelectList(db.Pacientes, "IdPaciente", "Nombre");
+            return View();
+        }
+
+        // POST: Ingresos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdIngreso,IdPacientes,IdHabitaciones,FechaIngreso")] Ingresos ingresos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ingresos.Add(ingresos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdHabitaciones = new SelectList(db.Habitaciones, "IdHabitacion", "Numero", ingresos.IdHabitaciones);
+            ViewBag.IdPacientes = new SelectList(db.Pacientes, "IdPaciente", "Nombre", ingresos.IdPacientes);
+            return View(ingresos);
+        }
+
+        // GET: Ingresos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingresos ingresos = db.Ingresos.Find(id);
+            if (ingresos == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdHabitaciones = new SelectList(db.Habitaciones, "IdHabitacion", "Tipo", ingresos.IdHabitaciones);
+            ViewBag.IdPacientes = new SelectList(db.Pacientes, "IdPaciente", "Cedula", ingresos.IdPacientes);
+            return View(ingresos);
+        }
+
+        // POST: Ingresos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdIngreso,IdPacientes,IdHabitaciones,FechaIngreso")] Ingresos ingresos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ingresos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdHabitaciones = new SelectList(db.Habitaciones, "IdHabitacion", "Tipo", ingresos.IdHabitaciones);
+            ViewBag.IdPacientes = new SelectList(db.Pacientes, "IdPaciente", "Cedula", ingresos.IdPacientes);
+            return View(ingresos);
+        }
+
+        // GET: Ingresos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingresos ingresos = db.Ingresos.Find(id);
+            if (ingresos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingresos);
+        }
+
+        // POST: Ingresos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ingresos ingresos = db.Ingresos.Find(id);
+            db.Ingresos.Remove(ingresos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
