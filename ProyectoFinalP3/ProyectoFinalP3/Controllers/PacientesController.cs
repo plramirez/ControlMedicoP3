975b3d005536bd3534fca2fb23846d96bcaf2964
Pedrito <pedroluisramirezo@gmail.com>
2020-03-31 20:40:49 -0400

    Creado CRUD de Pacientes.

---
+++ ProyectoFinalP3/ProyectoFinalP3/Controllers/PacientesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinalP3.Models;
+
+namespace ProyectoFinalP3.Controllers
+{
+    public class PacientesController : Controller
+    {
+        private ControlMedicoContext db = new ControlMedicoContext();
+
+        // GET: Pacientes
+        public ActionResult Index()
+        {
+            return View(db.Pacientes.ToList());
+        }
+
+        // GET: Pacientes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pacientes pacientes = db.Pacientes.Find(id);
+            if (pacientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pacientes);
+        }
+
+        // GET: Pacientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pacientes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdPaciente,Cedula,Nombre,Seguro")] Pacientes pacientes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pacientes.Add(pacientes);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pacientes);
+        }
+
+        // GET: Pacientes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pacientes pacientes = db.Pacientes.Find(id);
+            if (pacientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pacientes);
+        }
+
+        // POST: Pacientes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdPaciente,Cedula,Nombre,Seguro")] Pacientes pacientes)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pacientes).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pacientes);
+        }
+
+        // GET: Pacientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pacientes pacientes = db.Pacientes.Find(id);
+            if (pacientes == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pacientes);
+        }
+
+        // POST: Pacientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pacientes pacientes = db.Pacientes.Find(id);
+            db.Pacientes.Remove(pacientes);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
