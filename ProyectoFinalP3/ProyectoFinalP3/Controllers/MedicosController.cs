1b23fd78c4989a245b10c76f67d82aae12ee33a8
Pedrito <pedroluisramirezo@gmail.com>
2020-03-31 20:33:21 -0400

    Creado el CRUD para Medicos.

---
+++ ProyectoFinalP3/ProyectoFinalP3/Controllers/MedicosController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinalP3.Models;
+
+namespace ProyectoFinalP3.Controllers
+{
+    public class MedicosController : Controller
+    {
+        private ControlMedicoContext db = new ControlMedicoContext();
+
+        // GET: Medicos
+        public ActionResult Index()
+        {
+            return View(db.Medicos.ToList());
+        }
+
+        // GET: Medicos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicos medicos = db.Medicos.Find(id);
+            if (medicos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicos);
+        }
+
+        // GET: Medicos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Medicos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdMedico,Nombre,Exequatur,Especialidad")] Medicos medicos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Medicos.Add(medicos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(medicos);
+        }
+
+        // GET: Medicos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicos medicos = db.Medicos.Find(id);
+            if (medicos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicos);
+        }
+
+        // POST: Medicos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdMedico,Nombre,Exequatur,Especialidad")] Medicos medicos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(medicos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(medicos);
+        }
+
+        // GET: Medicos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Medicos medicos = db.Medicos.Find(id);
+            if (medicos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(medicos);
+        }
+
+        // POST: Medicos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Medicos medicos = db.Medicos.Find(id);
+            db.Medicos.Remove(medicos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
